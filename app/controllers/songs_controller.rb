226f3d08b9436660dca56d9a6d3c62a1470b6dfe
Prelang Builder <builder@prelang.com>
2014-07-03 23:19:09 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,8 @@
 class SongsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
   # GET /songs
